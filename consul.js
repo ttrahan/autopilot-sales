753a7c0b942c45b7c897b89470c18886c28d8d67
Tom Trahan <tom@shippable.com>
2016-05-25 23:52:52 -0700

    remove mysql backend

--- consul.js
+++ consul.js
@@ -1,7 +1,7 @@
 var exports = module.exports = {};
 var http = require('http');
 
-var CONSUL_DNS = 'consul.svc.b71934f2-d224-cd47-fd4b-ef25fd4ee85f.us-east-1.triton.zone'; //'consul'; // '72.2.114.55';
+var CONSUL_DNS = 'consul.svc.b71934f2-d224-cd47-fd4b-ef25fd4ee85f.us-east-1.triton.zone';
 
 // query Consul for the upstream services
 exports.getUpstreams = function(service, callback) {
