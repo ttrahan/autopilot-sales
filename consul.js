28d704a143e1d9e09651a70ed05b499987e73cb0
Tom Trahan <tom@shippable.com>
2016-05-25 20:44:53 -0700

    update consul location on triton

--- consul.js
+++ consul.js
@@ -1,10 +1,12 @@
 var exports = module.exports = {};
 var http = require('http');
 
+var CONSUL_HOSTS = 'consul.svc.b71934f2-d224-cd47-fd4b-ef25fd4ee85f.us-east-1.triton.zone'; //'consul'; // '72.2.114.55';
+
 // query Consul for the upstream services
 exports.getUpstreams = function(service, callback) {
   http.get({
-    host: 'consul',
+    host: CONSUL_HOSTS,
     port: 8500,
     path: '/v1/catalog/service/' + service
   }, function(response) {
